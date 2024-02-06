334e3cdda528241e111a0667b49a8973a96cf91b
Quy Do <quy.do@vertexgraph.com>
2024-02-05 20:38:28 -0500

    update controllers, client object and build

--- DotNet/GCalendar/Controllers/CalendarsController.cs
+++ DotNet/GCalendar/Controllers/CalendarsController.cs
@@ -1,6 +1,77 @@
-﻿namespace GCalendar.Controllers
+﻿using GCalendar.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static Google.Apis.Requests.BatchRequest;
+using System.Threading.Tasks;
+using System;
+using GCalendar.Helpers;
+
+namespace GCalendar.Controllers
 {
-    public class CalendarsController
+    public class CalendarsController : CalendarsHelpers
     {
+        #region Clear Calendar
+        [HttpPost("clear"), HttpPost("~/skill/{controller}/clear")]
+        public async Task<ServerResponse> ClearCalendar(ClearCalendarRequest Para)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Calendars][ClearCalendar]");
+            var response = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string Token = GetSessionToken();
+            if (!Has(Token))
+            {
+                Response.StatusCode = 401;
+                response.Message = "Unauthorized.";
+                return response;
+            }
+            try
+            {
+                response = await DeleteAllEventsFromCalendar(Para);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                response.Message = ex.Message;
+                return response;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Calendars][ClearCalendar]response:" + JsonConvert.SerializeObject(response));
+
+            return response;
+        }
+        #endregion
+
+        #region Remove Calendar
+        [HttpPost("remove"), HttpPost("~/skill/{controller}/remove")]
+        public async Task<ServerResponse> RemoveCalendar(RemoveCalendarRequest Para)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Calendars][RemoveCalendar]");
+            var response = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string Token = GetSessionToken();
+            if (!Has(Token))
+            {
+                Response.StatusCode = 401;
+                response.Message = "Unauthorized.";
+                return response;
+            }
+            try
+            {
+                response = await DeleteCalendar(Para);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                response.Message = ex.Message;
+                return response;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Calendars][RemoveCalendar]response:" + JsonConvert.SerializeObject(response));
+
+            return response;
+        }
+        #endregion
     }
 }
---
+++ DotNet/GCalendar/GCalendarClient/GCalendarListEventsMessage.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System;
+using GCalendar.Contracts;
+
+namespace GCalendar.GCalendarClient
+{
+    public class GCalendarListEventsMessage
+    {
+        [JsonProperty("kind"), JsonPropertyName("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("etag"), JsonPropertyName("etag")]
+        public string ETag { get; set; }
+
+        [JsonProperty("summary"), JsonPropertyName("summary")]
+        public string Summary { get; set; }
+
+        [JsonProperty("description"), JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("updated"), JsonPropertyName("updated")]
+        public DateTime Updated { get; set; }
+
+        [JsonProperty("timeZone"), JsonPropertyName("timeZone")]
+        public string TimeZone { get; set; }
+
+        [JsonProperty("accessRole"), JsonPropertyName("accessRole")]
+        public string AccessRole { get; set; }
+
+        [JsonProperty("defaultReminders"), JsonPropertyName("defaultReminders")]
+        public List<DefaultReminder> DefaultReminders { get; set; }
+
+        [JsonProperty("nextPageToken"), JsonPropertyName("nextPageToken")]
+        public string NextPageToken { get; set; }
+
+        [JsonProperty("nextSyncToken"), JsonPropertyName("nextSyncToken")]
+        public string NextSyncToken { get; set; }
+
+        [JsonProperty("items"), JsonPropertyName("items")]
+        public List<Event> Items { get; set; }
+
+        public class DefaultReminder
+        {
+            [JsonProperty("method"), JsonPropertyName("method")]
+            public string Method { get; set; }
+
+            [JsonProperty("minutes"), JsonPropertyName("minutes")]
+            public int Minutes { get; set; }
+        }
+    }
+}
