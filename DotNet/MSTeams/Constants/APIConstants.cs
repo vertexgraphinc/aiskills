3b3bd9d6159242a0157f1cd135a759cbac18f018
Quy Do <quy.do@vertexgraph.com>
2024-03-08 12:32:11 -0500

    update api base endpoint

--- DotNet/MSTeams/Constants/APIConstants.cs
+++ DotNet/MSTeams/Constants/APIConstants.cs
@@ -3,7 +3,7 @@
     public static class APIConstants
     {
         public const string GraphApiAuthURL = "https://login.microsoftonline.com/";
-        public const string GraphApiBaseURL = "https://graph.microsoft.com/v1.0/me/";
+        public const string GraphApiBaseURL = "https://graph.microsoft.com/v1.0/";
         public const string GraphApiScope = "User.Read User.ReadBasic.All Chat.ReadWrite ChatMember.ReadWrite Group.ReadWrite.All TeamMember.ReadWrite.All Team.ReadBasic.All";
     }
 }
