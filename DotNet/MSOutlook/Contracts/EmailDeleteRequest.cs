019829af6267615a99e74656774cda2f40eb52f4
Quy Do <quy.do@vertexgraph.com>
2024-03-27 19:01:02 -0400

    remove id contracts and add search and operation combination contracts

---
+++ DotNet/MSOutlook/Contracts/EmailDeleteRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace MSOutlook.Contracts
+{
+    public class EmailDeleteRequest
+    {
+        [JsonProperty("from")]
+        [JsonPropertyName("from")]
+        public string From { get; set; }
+
+        [JsonProperty("to")]
+        [JsonPropertyName("to")]
+        public string To { get; set; }
+
+        [JsonProperty("subject")]
+        [JsonPropertyName("subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("body")]
+        [JsonPropertyName("body")]
+        public string Body { get; set; }
+    }
+}
