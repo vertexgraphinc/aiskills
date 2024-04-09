421bd580c352b278cd4818c5e40d8af94f26d3d9
Quy Do <quy.do@vertexgraph.com>
2024-04-10 06:07:02 +0700

    add meeting query contracts for chat

---
+++ DotNet/Zoom/Contracts/MeetingChatResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Zoom.Contracts
+{
+    public class MeetingChatResponse
+    {
+        [JsonProperty("meetingTopic")]
+        [JsonPropertyName("meetingTopic")]
+        public string MeetingTopic { get; set; }
+
+        [JsonProperty("meetingStartTime")]
+        [JsonPropertyName("meetingStartTime")]
+        public string MeetingStartTime { get; set; }
+
+        [JsonProperty("meetingDuration")]
+        [JsonPropertyName("meetingDuration")]
+        public int MeetingDuration { get; set; }
+
+        [JsonProperty("meetingType")]
+        [JsonPropertyName("meetingType")]
+        public int MeetingType { get; set; }
+
+        [JsonProperty("downloadUrl")]
+        [JsonPropertyName("downloadUrl")]
+        public string DownloadUrl { get; set; }
+    }
+}
---
+++ DotNet/Zoom/Contracts/MeetingChatsQueryResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Zoom.Contracts
+{
+    public class MeetingChatsQueryResponse : ServerResponse
+    {
+        [JsonProperty("chats")]
+        [JsonPropertyName("chats")]
+        public List<MeetingChatResponse> Chats { get; set; }
+    }
+}
