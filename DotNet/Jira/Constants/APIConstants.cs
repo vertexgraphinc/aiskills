2578aa4f74efbc19072d55047fd1c51f8b71c63b
Zain Afzal <zainafzal52@gmail.com>
2024-04-15 16:24:31 -0400

    Fix Jira API Scopes

---
+++ DotNet/Jira/Constants/APIConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Jira.Constants
+{
+    public static class APIConstants
+    {
+        public const string ApiAuthURL = "https://auth.atlassian.com/oauth/token";
+        public const string ApiBaseURL = "https://auth.atlassian.com/authorize";
+        public const string ApiScope = "read:jira-work write:jira-work read:jira-user";
+    }
+}
