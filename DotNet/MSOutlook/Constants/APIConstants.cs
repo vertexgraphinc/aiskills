be902fe228c9569c4006395cd916bcdc889fbde5
valterkgladinet <valterk@centrestack.com>
2024-04-18 11:24:50 -0400

    AI-635-Outlook Skill - missing refresh token after initial authentication

--- DotNet/MSOutlook/Constants/APIConstants.cs
+++ DotNet/MSOutlook/Constants/APIConstants.cs
@@ -4,6 +4,6 @@
     {
         public const string GraphApiAuthURL = "https://login.microsoftonline.com/";
         public const string GraphApiBaseURL = "https://graph.microsoft.com/v1.0/me/";
-        public const string GraphApiScope = "User.Read Mail.ReadWrite Mail.Send";
+        public const string GraphApiScope = "offline_access User.Read Mail.ReadWrite Mail.Send";
     }
 }
