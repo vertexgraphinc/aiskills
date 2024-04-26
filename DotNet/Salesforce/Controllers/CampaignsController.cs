ff899907411b9291270a1b8118bfd27f5193087e
Quy Do <quy.do@vertexgraph.com>
2024-04-26 14:46:42 +0700

    update controller implementation

---
+++ DotNet/Salesforce/Controllers/CampaignsController.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Salesforce.Contracts;
+using Salesforce.Helpers;
+using Salesforce.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace Salesforce.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CampaignsController : ControllerBase
+    {
+        private readonly ICampaignService _campaignService;
+
+        public CampaignsController(ICampaignService campaignService)
+        {
+            _campaignService = campaignService;
+        }
+
+        [HttpPost("query")]
+        public async Task<CampaignsQueryResponse> QueryCampaigns(CampaignsQueryRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Query]");
+            CampaignsQueryResponse resp = new CampaignsQueryResponse
+            {
+                Campaigns = null
+            };
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                resp.Campaigns = await _campaignService.QueryCampaigns(request, token);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Query]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+
+        [HttpPost("create")]
+        public async Task<CampaignCreateResponse> CreateCampaign(CampaignCreateRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Create]");
+            CampaignCreateResponse resp = new CampaignCreateResponse
+            {
+                Campaign = null
+            };
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                resp.Campaign = await _campaignService.CreateCampaign(request, token);
+                if (resp.Campaign != null)
+                {
+                    resp.Message = "Campaign created successfully.";
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    resp.Message = "Failed to create campaign.";
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Create]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+
+        [HttpPost("update")]
+        public async Task<ServerResponse> UpdateCampaigns(CampaignsUpdateRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Update]");
+            var resp = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                bool isUpdated = await _campaignService.UpdateCampaigns(request, token);
+                if (isUpdated)
+                {
+                    resp.Message = "Campaign updated successfully.";
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    resp.Message = "Failed to update campaign.";
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Update]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+
+        [HttpPost("remove")]
+        public async Task<ServerResponse> RemoveCampaigns(CampaignsRemoveRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Remove]");
+            var resp = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                bool isUpdated = await _campaignService.RemoveCampaigns(request, token);
+                if (isUpdated)
+                {
+                    resp.Message = "Campaign removed successfully.";
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    resp.Message = "Failed to remove campaign.";
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Campaigns][Remove]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+    }
+}
---
+++ DotNet/Salesforce/Controllers/ContactsController.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Salesforce.Contracts;
+using Salesforce.Helpers;
+using Salesforce.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace Salesforce.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpPost("query")]
+        public async Task<ContactsQueryResponse> QueryContacts(ContactsQueryRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Query]");
+            ContactsQueryResponse resp = new ContactsQueryResponse
+            {
+                Contacts = null
+            };
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                resp.Contacts = await _contactService.QueryContacts(request, token);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Query]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+
+        [HttpPost("create")]
+        public async Task<ServerResponse> CreateContact(ContactCreateRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Create]");
+            var resp = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                bool isCreated = await _contactService.CreateContact(request, token);
+                if (isCreated)
+                {
+                    resp.Message = "Contact created successfully.";
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    resp.Message = "Failed to create contact.";
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Create]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+
+        [HttpPost("update")]
+        public async Task<ServerResponse> UpdateContacts(ContactsUpdateRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Update]");
+            var resp = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                bool isUpdated = await _contactService.UpdateContacts(request, token);
+                if (isUpdated)
+                {
+                    resp.Message = "Contact updated successfully.";
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    resp.Message = "Failed to update contact.";
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Update]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+
+        [HttpPost("remove")]
+        public async Task<ServerResponse> RemoveContacts(ContactsRemoveRequest request)
+        {
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Remove]");
+            var resp = new ServerResponse();
+            Response.StatusCode = 200;
+
+            string authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+            string token = TokenHelper.GetSessionToken(authorizationHeader);
+            if (string.IsNullOrEmpty(token))
+            {
+                Response.StatusCode = 401;
+                resp.Message = "Unauthorized.";
+                return resp;
+            }
+
+            try
+            {
+                bool isUpdated = await _contactService.RemoveContacts(request, token);
+                if (isUpdated)
+                {
+                    resp.Message = "Contact removed successfully.";
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    resp.Message = "Failed to remove contact.";
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                resp.Message = e.Message;
+                return resp;
+            }
+
+            System.Diagnostics.Debug.WriteLine("[vertex][Contacts][Remove]response:" + JsonConvert.SerializeObject(resp));
+            return resp;
+        }
+    }
+}
